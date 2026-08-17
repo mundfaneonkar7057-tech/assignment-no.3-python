6e6c135552c4300e780a0110fe4434d47422fb51
Onkar Mundfane <mundfaneonkar7057@gmail.com>
2026-08-17 12:43:34 +0530

    python assignment no .3

---
+++ assignment no3.py
@@ -0,0 +1,18 @@
+# create a list of assignment
+
+assignments =['dsa assignment','dms assignment','c++ assifnment']
+print(assignments)
+
+# adding new assignment in list
+
+assignments.append('python assignment')
+print(assignments)
+
+#remove the assignments & print
+
+assignments.remove('dsa assignment')
+print(assignments)
+
+# print updater assifnment list
+print("print updated assignments list")
+print(assignments)
