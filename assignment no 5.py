17c6ba7fa9fce8b9950d5f57c378b762b2f86491
Onkar Mundfane <mundfaneonkar7057@gmail.com>
2026-08-27 10:34:25 +0530

    Create assignment no 5.py

---
+++ assignment no 5.py
@@ -0,0 +1,41 @@
+print("datta")
+#first student
+st=int(input("enter the first student roll no"))
+s1=int(input("enter the marks of s1 sub:"))
+print("mark ",s1)
+s2=int(input("enter the marks of s2 sub:"))
+print("mark ",s2)
+s3=int(input("enter the marks of s3 sub:"))
+print("mark ",s3)
+s4=int(input("enter the marks of s4 sub:"))
+print("mark ",s4)
+s5=int(input("enter the marks of s5 sub:"))
+print("mark ",s5)
+
+#2 second student
+print("onkar")
+st=int(input("enter the second student roll no"))
+s1=int(input("enter the marks of s1 sub:"))
+print("mark ",s1)
+s2=int(input("enter the marks of s2 sub:"))
+print("mark ",s2)
+s3=int(input("enter the marks of s3 sub:"))
+print("mark ",s3)
+s4=int(input("enter the marks of s4 sub:"))
+print("mark ",s4)
+s5=int(input("enter the marks of s5 sub:"))
+
+
+# avg  firs student 
+
+avg1=(s1+s2+s3+s4+s5)/5
+print("first studen avg",avg1 )
+
+#avg second studnet
+
+avg2=(s1+s2+s3+s4+s5)/5
+print("second studtn avg",avg2)
+
+overall =(avg1+avg2) /2
+print("overall aveg of",overall)
+
